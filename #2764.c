d61b95a98fb10e543e1ba45f8e5358a79d64272d
nabil10000 <144065318+nabil10000@users.noreply.github.com>
2023-09-05 02:59:15 +0600

    Update #2764.c

--- #2764.c
+++ #2764.c
@@ -6,8 +6,5 @@ scanf("%c %c%*c%c%c%*c%c %c",&a,&b,&c,&d,&e,&f);
 printf("%c%c/%c%c/%c%c\n",c,d,a,b,e,f);
 printf("%c%c/%c%c/%c%c\n",e,f,c,d,a,b);
 printf("%c%c-%c%c-%c%c\n",a,b,c,d,e,f);
-
 return 0 ;
-
-
 }
