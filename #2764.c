2fade65ec2c2f5bbb1fb4c1ef4429bafbcbadc2f
nabil10000 <144065318+nabil10000@users.noreply.github.com>
2023-09-05 02:58:18 +0600

    problem-#216

---
+++ #2764.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main ()
+{
+char a,b,c,d,e,f;
+scanf("%c %c%*c%c%c%*c%c %c",&a,&b,&c,&d,&e,&f);
+printf("%c%c/%c%c/%c%c\n",c,d,a,b,e,f);
+printf("%c%c/%c%c/%c%c\n",e,f,c,d,a,b);
+printf("%c%c-%c%c-%c%c\n",a,b,c,d,e,f);
+
+return 0 ;
+
+
+}
