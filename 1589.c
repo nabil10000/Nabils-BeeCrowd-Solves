3583ae1510ed804d65ff5c0bf4be9c19b5045bc3
Fazle Rabbi Nabil <144065318+nabil10000@users.noreply.github.com>
2023-11-22 02:13:58 +0600

    Add files via upload

---
+++ 1589.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int a,r1,r2;
+    scanf("%d",&a);
+    while(a--){
+            scanf("%d %d",&r1,&r2);
+        printf("%d\n",r1+r2);
+    }
+    return 0;
+}
+
---
+++ 1930.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int t1,t2,t3,t4,tt,t;
+
+    scanf("%d %d %d %d",&t1,&t2,&t3,&t4);
+
+     printf("%d\n",(t1+t2+t3+t4)-3);
+
+    return 0;
+}
