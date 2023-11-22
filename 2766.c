c6bb2daa650f4eb426af72e96b3e07ddba19d64c
Fazle Rabbi Nabil <144065318+nabil10000@users.noreply.github.com>
2023-11-22 10:29:54 +0600

    Add files via upload

---
+++ 2766.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+
+    char string[40];
+
+    for (int i = 1; i <= 10; ++i)
+    {
+
+        scanf("%s", string);
+
+        if (i == 3)
+            puts(string);
+        else if (i == 7)
+            puts(string);
+        else if (i == 9)
+            puts(string);
+
+    }
+
+    return 0;
+
+}
