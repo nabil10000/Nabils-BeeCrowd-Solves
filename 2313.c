d5a5ae532ff8ec422f5c8acec2a93cac7d927a19
Fazle Rabbi Nabil <144065318+nabil10000@users.noreply.github.com>
2023-12-17 04:15:17 +0600

    Update 2313.c

--- 2313.c
+++ 2313.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+
+    // Check if the input values can form a triangle
+    if (a + b > c && a + c > b && b + c > a) {
+        printf("Valido-");
+        
+        // Determine triangle type
+        if (a == b && b == c) {
+            printf("Equilatero\n");
+        } else if (a == b || b == c || a == c) {
+            printf("Isoceles\n");
+        } else {
+            printf("Escaleno\n");
+        }
+        
+        // Check if it's a right-angled triangle
+        if (a * a == b * b + c * c || b * b == a * a + c * c || c * c == a * a + b * b) {
+            printf("Retangulo: S\n");
+        } else {
+            printf("Retangulo: N\n");
+        }
+    } else {
+        printf("Invalido\n");
+    }
+
+    return 0;
+}
 
